b926ba3bf5e4f566ea4a6b1c9dd38071e64f3493
thameempn <mohammedthameem2002@gmail.com>
2024-06-25 10:40:14 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev Social Media App",
+  title: "Connectify",
   description: "Social media app built with Next.js",
 };
 
